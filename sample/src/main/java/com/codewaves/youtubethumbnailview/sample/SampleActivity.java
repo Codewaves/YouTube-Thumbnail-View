00a03197f1526804a965e014056434b682334f8e
Sergej Kravcenko <skravcenko@codewaves.com>
2017-04-15 02:25:03 +0300

    Add simple sample

---
+++ sample/src/main/java/com/codewaves/youtubethumbnailview/sample/SampleActivity.java
@@ -0,0 +1,18 @@
+package com.codewaves.youtubethumbnailview.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sergej Kravcenko on 4/14/2017.
+ * Copyright (c) 2017 Sergej Kravcenko
+ */
+
+public class SampleActivity extends AppCompatActivity {
+   @Override
+   protected void onCreate(@Nullable Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_sample);
+   }
+}
